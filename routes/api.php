7ceb71252944fa45ebf592c1660ced4602e8c725
Jesse-js <jesse-jjs@hotmail.com>
2024-06-12 18:16:48 -0300

    feat: adds show method to api

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::prefix('v1')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
 });
 
